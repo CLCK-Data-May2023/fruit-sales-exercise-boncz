c192c2b3b06a3fa3854c98d6141fb8f7a494a97a
Ailene Bonczkowski <a.boncz@gmail.com>
2023-08-10 13:42:40 -0400

    second commit.

--- fruitsales.py
+++ fruitsales.py
@@ -3,3 +3,5 @@ import pandas as pd
 df_fruit_sales = pd.DataFrame({'Apples':[35,41], 'Bananas':[21,34]}, index=['2017 Sales','2018 Sales'])
 
 df_fruit_sales.to_csv('fruit.csv')
+
+print(df_fruit_sales)
